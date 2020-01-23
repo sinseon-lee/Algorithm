3792e10fa45862bdb2fd7a876cca262551043d31
Sinseon Lee <sinseonky@gmail.com>
2020-01-23 17:31:52 +0900

    clear problem 3 2018

--- 2018/3. Cache/python/code.py
+++ 2018/3. Cache/python/code.py
@@ -1,28 +1,28 @@
-#
+# Calculate the running time
 def measurer(cacheSize, cities):
     time = 0
     cache = []
 
+    # make all characters lower case as it's non-case-sensative
     for i in range(len(cities)):
         cities[i] = cities[i].lower()
 
     while (len(cities) != 0):
-        
         city = cities[0]
+        
         if (city in cache):
             time += 1
         else:
             time += 5
 
         cache.append(city)
+        # to fix the cache size
         if (len(cache) > cacheSize):
             cache = cache[1:]
 
         cities = cities[1:]
         
     return time
-
-
 
 # Test 1
 cacheSize = 3
