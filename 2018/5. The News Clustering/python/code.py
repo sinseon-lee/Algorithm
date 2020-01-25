6c91b140abcf8a8a404ff049cba175fb1dafe662
Sinseon Lee <sinseonky@gmail.com>
2020-01-25 21:14:27 +0900

    clear problem 5 2020 in python

---
+++ 2018/5. The News Clustering/python/code.py
@@ -0,0 +1,68 @@
+def similarity(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
+    str1Alphabet = ""
+    str2Alphabet= ""
+
+    alphabetList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    set1 = []
+    set2 = []
+
+    for i in range(len(str1) - 1):
+        if (str1[0] in alphabetList and str1[1] in alphabetList):
+            set1.append(str1[:2])
+        str1 = str1[1:]
+    for i in range(len(str2) -1):
+        if (str2[0] in alphabetList and str2[1] in alphabetList):
+            set2.append(str2[:2])
+        str2 = str2[1:]
+    
+    intersection = []
+    union = []
+    while (len(set1) != 0):
+        element = set1[0]
+        if (element in set2):
+            intersection.append(element)
+            union.append(element)
+            set1.remove(element)
+            set2.remove(element)
+        else:
+            union.append(element)
+            set1.remove(element)
+    
+    while (len(set2) !=0):
+        element = set2[0]
+        union.append(element)
+        set2.remove(element)
+
+    if (len(union) == 0):
+        jaccardSimilarity = 1 * 65536
+    else:
+        jaccardSimilarity = int(len(intersection) / len(union) * 65536)
+
+
+
+    return jaccardSimilarity
+
+
+
+# Test 1
+str1 = "FRANCE"
+str2 = "french"
+print(similarity(str1, str2))
+
+# Test 2
+str1 = "handshake"
+str2 = "shake hands"
+print(similarity(str1, str2))
+
+# Test 3
+str1 = "aa1+aa2"
+str2 = "AAAA12"
+print(similarity(str1, str2))
+
+# Test 4
+str1 = "E=M*C^2"
+str2 = "e=m*c^2"
+print(similarity(str1, str2))
